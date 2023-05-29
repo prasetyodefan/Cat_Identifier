60d3e86a79df37c87299284c503f65959412556c
prasetyodefan <prasetyodefan@gmail.com>
2023-05-30 01:42:52 +0700

    tuning classifier

--- identifiersavepred.py
+++ identifiersavepred.py
@@ -1,29 +1,32 @@
 ## Preprocessing step 1 - filtered images and xml files
 import os
+import time
+start_pascal = time.time()
 
 img_names = []
 xml_names = []
 
-for dirname, subdirs, filenames in os.walk('asset/dataset/mix/'):
+for dirname, subdirs, filenames in os.walk('asset/dataset/td/'):
   for filename in filenames:
     if filename[-3:] != "xml":
       img_names.append(filename)
     else:
       xml_names.append(filename)
 
-print(" Total files")
-print(len(img_names), "images")
-print(len(xml_names), "xml files")
+print("Total Files")
+print("Images    :",len(img_names))
+print("Xml Files :",len(xml_names))
+print()
 
 ## Preprocessing step 2 - cropped images by bounding box using xml files 
 import xmltodict
 from matplotlib import pyplot as plt
 from skimage.io import imread
 
-path_annotations = "asset/dataset/mix/"
-path_images = "asset/dataset/mix/"
-
-class_names = ['bengal','rblue','siamese','ragdoll']
+path_annotations = "asset/dataset/td/"
+path_images = "asset/dataset/td/"
+# 'bengal','persian','siamese','ragdoll','rblue'
+class_names = ['bengal','siamese','persian']
 images = []
 target = []
 
@@ -53,17 +56,27 @@ for img_name in img_names:
     target.append(temp["name"])
 
 # print total target by class
-print("# Total target by class")
+print("Total target by class")
 for i in class_names:
-  print(i, ":", target.count(i))
+  print(i , "   :", target.count(i))
 
+# --------------------------------------------------------------------
+end_pascal = time.time()
+pascalex = end_pascal - start_pascal
+print("Execution time Read Pascal : {} seconds".format(pascalex) ,"\n" )
 
+# --------------------------------------------------------------------
+
+start_prepo = time.time()
 ## Preprocessing step 3 - resize images to 258x258 and normalize (remove background and grayscale)
 import numpy as np
 import skimage
 from skimage.transform import resize
 
-def resize_image(img, size=128):
+from scipy import ndimage
+from skimage import io, color, exposure, filters
+
+def resize_image(img, size=32):
   _img = img.copy() 
   _img = resize(_img, (size, size))
   return _img
@@ -79,10 +92,30 @@ def grayscale(img):
   _img = np.dot(_img[...,:3], [0.299, 0.587, 0.114])
   return _img
 
+def prepo(img):
+  _img = img.copy()
+
+  # Apply Adaptive Histogram Equalization (AHE) to the grayscale image
+  clahe_image = exposure.equalize_adapthist(_img , clip_limit=0.03)
+
+  # Apply Unsharp Masking to the AHE result
+  blurred = ndimage.gaussian_filter(clahe_image, sigma=1)
+  unsharp_mask = clahe_image - 0.7 * blurred
+
+  # Perform segmentation using Thresholding
+  threshold_value = filters.threshold_otsu(unsharp_mask)
+  binary_image = unsharp_mask > threshold_value
+
+  # Convert the binary image to uint8 and scale it to 0-255
+  binary_image = np.uint8(binary_image) * 255
+  return _img  
+
+
 for i in range(len(images)):
   images[i] = resize_image(images[i])
   images[i] = grayscale(images[i])
-  images[i] = remove_background(images[i])
+  images[i] = prepo(images[i])
+  # images[i] = remove_background(images[i])
 
 ## Extration step 1 - extract features using PHOG (Pyramid Histogram of Oriented Gradients)
 from PIL import Image
@@ -161,27 +194,39 @@ def phog(img, bin_size=16, levels=3):
 
 features = []
 for i in range(len(images)):
-  print("Processing item", i+1, "of ",len(images),"...")
+  print("Processing", i+1, "of",len(images))
   features.append(phog(images[i]))
 
-# print(features[0])
+# --------------------------------------------------------------------
+end_prepo = time.time()
+prepotime = end_prepo - start_prepo
+print()
+print("Execution time Prepp : {} seconds".format(prepotime))
+# --------------------------------------------------------------------
 
+start_split = time.time()
 from sklearn.model_selection import train_test_split
 
 X, y = features, np.array(target)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+print()
+print("Training Data :\n", np.asarray(np.unique(y_train, return_counts=True)).T)
+print("Test Data     :\n", np.asarray(np.unique(y_test, return_counts=True)).T)
 
-print("Training data\n", np.asarray(np.unique(y_train, return_counts=True)).T)
-print("Test data\n", np.asarray(np.unique(y_test, return_counts=True)).T)
+end_split = time.time()
+splittime = end_split - start_split
+print()
+print("Execution Time Split Data : {} seconds".format(splittime))
 
 ## Classification step 1
-
-
-# # # ------------------------------------------------------
+# # ------------------------------------------------------
+# #!             TEST TUNING CLASSIFIER
+# # ------------------------------------------------------
 # from sklearn.svm import SVC
 # from sklearn.tree import DecisionTreeClassifier
 # from sklearn.ensemble import StackingClassifier
 # from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import GridSearchCV
 
 # clf = StackingClassifier(
 #     estimators=[('svm', SVC(random_state=42)),
@@ -189,7 +234,7 @@ print("Test data\n", np.asarray(np.unique(y_test, return_counts=True)).T)
 #     final_estimator=LogisticRegression(random_state=42),
 #     n_jobs=-1)
 
-# from sklearn.model_selection import GridSearchCV
+
 
 # param_grid = {
 #     'svm__C': [1.6, 1.7, 1.8],
@@ -208,9 +253,14 @@ print("Test data\n", np.asarray(np.unique(y_test, return_counts=True)).T)
 # grid.fit(X_train, y_train)
 
 # print('Best parameters: %s' % grid.best_params_)
-# print('Accuracy: %.2f' % grid.best_score_)
-# # # ------------------------------------------------------
+# print('Accuracy       : %.2f' % grid.best_score_)
+# # ------------------------------------------------------
 
+
+# # ------------------------------------------------------
+# #!             RUN CLASSIFIER
+# # ------------------------------------------------------
+start_class = time.time()
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import StackingClassifier
@@ -218,62 +268,54 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 final_clf = StackingClassifier(
-    estimators=[('svm', SVC(C=1.8, kernel='rbf', random_state=42)),
-                ('tree', DecisionTreeClassifier(criterion='entropy', max_depth=9, random_state=42))],
-    final_estimator=LogisticRegression(C=1.5, random_state=42),
+    estimators=[('svm', SVC(C=1.6, kernel='rbf', random_state=42)),
+                ('tree', DecisionTreeClassifier(criterion='entropy', 
+                                                max_depth=9, random_state=42))],
+    final_estimator=LogisticRegression(C=1.3, random_state=42),
     n_jobs=-1)
 
 final_clf.fit(X_train, y_train)
 y_pred = final_clf.predict(X_test)
 
-print('Accuracy score : ', accuracy_score(y_test, y_pred))
-print('Precision score : ', precision_score(y_test, y_pred, average='weighted'))
-print('Recall score : ', recall_score(y_test, y_pred, average='weighted'))
-print('F1 score : ', f1_score(y_test, y_pred, average='weighted'))
+print()
+print('Accuracy score   : ', accuracy_score(y_test, y_pred))
+print('Precision score  : ', precision_score(y_test, y_pred, average='weighted'))
+print('Recall score     : ', recall_score(y_test, y_pred, average='weighted'))
+print('F1 score         : ', f1_score(y_test, y_pred, average='weighted'))
+end_class = time.time()
+classtime = end_class - start_class
+print()
+print("Execution time Classifier : {} seconds".format(classtime))
 
 
-# Save SVM Model
 
-import pickle
+# # ------------------------------------------------------
+# #!               SAVE CLASSIFIER MODEL
+# # ------------------------------------------------------
+# # import pickle
+# # pkl_filename = 'svm_model.pkl'
+# # with open(pkl_filename, 'wb') as file:
+# #   pickle.dump(final_clf, file)
 
-pkl_filename = 'svm_model.pkl'
-with open(pkl_filename, 'wb') as file:
-  pickle.dump(final_clf, file)
 
-# create confusion matrix
-
-from sklearn.metrics import confusion_matrix
-
-# cm = confusion_matrix(y_test, y_pred, labels=['ragdoll','siamese'])
-
-# print confusion matrix
-
+# # ------------------------------------------------------
+# #?             CONFUSSION MATRIX
+# # ------------------------------------------------------
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
-
-# # Plot non-normalized confusion matrix
-# titles_options = [
-#     ("Confusion matrix, without normalization", None),
-# ]
-# for title, normalize in titles_options:
-#     disp = ConfusionMatrixDisplay.from_estimator(
-#         final_clf,
-#         X_test,
-#         y_test,
-#         display_labels=class_names,
-#         cmap=plt.cm.Blues,
-#     )
-#     disp.ax_.set_title(title)
-
-#     print(title)
-#     print(disp.confusion_matrix)
-
-# plt.show()
 from sklearn.metrics import classification_report
-print("Predicting people's names on the test set")
-print(classification_report(y_test, y_pred, target_names=class_names))
+# print("Predicting cat breed on the test set")
+print()
+print(classification_report(y_test, 
+                            y_pred, 
+                            target_names=class_names
+                            ))
 ConfusionMatrixDisplay.from_estimator(
-    final_clf, X_test, y_test, display_labels=class_names, xticks_rotation="vertical"
+    final_clf, 
+    X_test, 
+    y_test,
+    display_labels=class_names, 
+    xticks_rotation="vertical" #,cmap=plt.cm.Blues
 )
 plt.tight_layout()
 plt.show()
